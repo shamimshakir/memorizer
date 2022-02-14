8f41a9a973fd173b518cbc8386eb91dd41fc623f
shamimshakir <shamimshakir75@gmail.com>
2022-02-14 16:15:45 +0600

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
